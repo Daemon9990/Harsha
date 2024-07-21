18c4357840b70939c9e5efd02f37e1087305e427
Daemon9990 <meruguvasu963@gmail.com>
2024-07-21 18:44:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ API_ID = os.environ.get("API_ID", "23270278")
 API_HASH = os.environ.get("API_HASH", "87a837607d9761cdf98bbc3e61eef098")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7238067007:AAG1pDJTUBwKzeeu0o-G0acMrQUA4JN9Zug")
 ADMIN = int(os.environ.get("ADMIN", '6890795853'))
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "VK_OFCL")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "")
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Teluguzoneofc")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "BujjiSupport")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://parcel:malliga@cluster0.xoax6ur.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 CAPTION = os.environ.get("CAPTION", "")
-group = environ.get('GROUP', '')
+group = environ.get('GROUP', '-1002191685064')
 GROUP = int(group) if group and id_pattern.search(group) else None
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 SUNRISES_PIC= "https://graph.org/file/cfb3eb77fd01bae0a2344.jpg"  # Replace with your Telegraph link
