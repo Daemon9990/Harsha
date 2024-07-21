fe26ba6604259c65e01181650a88748c92b62907
Daemon9990 <meruguvasu963@gmail.com>
2024-07-21 18:37:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,20 +6,20 @@ import os
 id_pattern = re.compile(r'^.\d+$')
 
 
-API_ID = os.environ.get("API_ID", "10811400")
-API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
-ADMIN = int(os.environ.get("ADMIN", '6469754522'))
-FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
-DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://UPLOADXPRO24BOT:UPLOADXPRO24BOT@cluster0.hjfk60f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+API_ID = os.environ.get("API_ID", "23270278")
+API_HASH = os.environ.get("API_HASH", "87a837607d9761cdf98bbc3e61eef098")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7238067007:AAG1pDJTUBwKzeeu0o-G0acMrQUA4JN9Zug")
+ADMIN = int(os.environ.get("ADMIN", '6890795853'))
+FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "VK_OFCL")
+FSUB_GROUP = os.environ.get("FSUB_GROUP", "")
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://parcel:malliga@cluster0.xoax6ur.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 CAPTION = os.environ.get("CAPTION", "")
-group = environ.get('GROUP', '-1002128043143')
+group = environ.get('GROUP', '')
 GROUP = int(group) if group and id_pattern.search(group) else None
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
-SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+SUNRISES_PIC= "https://graph.org/file/cfb3eb77fd01bae0a2344.jpg"  # Replace with your Telegraph link
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '809614790'))
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
 PORT = int(os.environ.get("PORT", "8080"))
-LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002145984196)
+LOG_CHANNEL_ID = os.environ.get("LOG_CHANNEL_ID", -1002157233898)
